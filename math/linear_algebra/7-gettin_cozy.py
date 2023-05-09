849398885d03377a0e0917cb4b80a39167d8a742
Julia Bullard <5534@holbertonstudents.com>
2023-05-09 17:56:27 -0500

    Task 7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""Task 7"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """Concatenates two matrices along a specific axis."""
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        return mat1 + mat2
+    elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        return [mat1[i] + mat2[i] for i in range(len(mat1))]
+    else:
+        return None
