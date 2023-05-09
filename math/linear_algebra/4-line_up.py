19fe105ea94f08cfc1e8e6714397d48618da3ee8
Julia Bullard <5534@holbertonstudents.com>
2023-05-09 17:43:12 -0500

    Task 4

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""Task 4"""
+
+
+def add_arrays(arr1, arr2):
+    """Adds two arrays element-wise."""
+    # Check if the arrays have the same length
+    if len(arr1) != len(arr2):
+        return None
+
+    # Create a new list to store the result
+    result = []
+
+    # Iterate over the arrays and add their elements
+    for i in range(len(arr1)):
+        result.append(arr1[i] + arr2[i])
+
+    # Return the resulting list
+    return result
