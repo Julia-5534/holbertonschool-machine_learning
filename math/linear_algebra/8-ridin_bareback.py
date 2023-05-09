bf5b89ae15ba0088944252883bf6a4d199e2b891
Julia Bullard <5534@holbertonstudents.com>
2023-05-09 17:59:36 -0500

    Task 8

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""Task 8"""
+
+
+def mat_mul(mat1, mat2):
+    """Performs matrix multiplication."""
+    if len(mat1[0]) != len(mat2):
+        return None
+    result = [[0 for j in range(len(mat2[0]))] for i in range(len(mat1))]
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            scalar_product = 0
+            for k in range(len(mat2)):
+                scalar_product += mat1[i][k] * mat2[k][j]
+            result[i][j] = scalar_product
+    return result
