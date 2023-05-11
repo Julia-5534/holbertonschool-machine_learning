2b951d4caa520f1cfb232648740edc92c4aaf489
Julia Bullard <5534@holbertonstudents.com>
2023-05-11 14:11:11 -0500

    Task 4 fixed

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,4 +9,11 @@ plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
 plt.xlabel('Grades')
 plt.ylabel('Number of Students')
 plt.title('Project A')
+
+# Set x and y axis ticks and limits
+plt.xticks(np.arange(0, 101, 10))
+plt.yticks(np.arange(0, 31, 5))
+plt.xlim(0, 100)
+plt.ylim(0, 30)
+
 plt.show()
