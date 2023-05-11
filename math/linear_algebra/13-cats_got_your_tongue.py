5740b337488d0848bbb81040a5a21300ad29f82e
Julia Bullard <5534@holbertonstudents.com>
2023-05-11 12:56:36 -0500

    Whoopsie on no import

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 """Task 13"""
+import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    """Concatenates two matrices on a given axis"""
-    funcs = {0: lambda a, b: a + b,
-             1: lambda a, b: [x + y for x, y in zip(a, b)]}
-    return funcs[axis]([list(row) for row in mat1],
-                       [list(row) for row in mat2])
+    """Concatenate two matrices on a given axis"""
+    return np.concatenate((mat1, mat2), axis=axis)
