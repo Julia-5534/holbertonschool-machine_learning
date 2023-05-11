2f772bbcb69c0749b1fd5a2af9daee0328edd7b8
Julia Bullard <5534@holbertonstudents.com>
2023-05-11 12:29:15 -0500

    Task 12

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""Task 12"""
+import numpy as np
+
+
+def np_elementwise(mat1, mat2):
+    """Element-wise +, -, *, /,"""
+    sum_ = np.add(mat1, mat2)
+    diff = np.subtract(mat1, mat2)
+    prod = np.multiply(mat1, mat2)
+    quot = np.divide(mat1, mat2)
+    return sum_, diff, prod, quot
