b022a85b1f0b5f31bfa5e726510527b9f7c0d28d
Julia Bullard <5534@holbertonstudents.com>
2023-05-11 12:49:34 -0500

    Task 13

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Task 13"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """Concatenates two matrices on a given axis"""
+    funcs = {0: lambda a, b: a + b,
+             1: lambda a, b: [x + y for x, y in zip(a, b)]}
+    return funcs[axis]([list(row) for row in mat1],
+                       [list(row) for row in mat2])
