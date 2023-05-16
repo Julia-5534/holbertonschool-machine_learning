b4ba8d890fdbfa77c21bdc471a7d7e4d2fb604ed
Julia Bullard <5534@holbertonstudents.com>
2023-05-15 22:20:08 -0500

    Task 17 again

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,11 +4,13 @@
 
 def poly_integral(poly, C=0):
     """Calculates the integral of a polynomial"""
-    if not isinstance(poly, list) or not all(isinstance(coeff, int) for coeff in poly) \
+    if not isinstance(poly, list) or not \
+        all(isinstance(coeff, int) for coeff in poly) \
             or not isinstance(C, int):
         return None
 
-    integral = [coeff // (index + 1) if coeff % (index + 1) == 0 else coeff / (index + 1)
+    integral = [coeff // (index + 1)
+                if coeff % (index + 1) == 0 else coeff / (index + 1)
                 for index, coeff in enumerate(poly)]
     integral.insert(0, C)
 
