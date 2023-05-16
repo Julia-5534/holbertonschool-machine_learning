4bff206d675b63b9fbb5f19944f651c3f215e5d9
Julia Bullard <5534@holbertonstudents.com>
2023-05-15 21:41:42 -0500

    No loops, whoops!

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -3,8 +3,8 @@
 
 
 def summation_i_squared(n):
-    """Calculates ""sum_{i=1}^{n} i^2"""
+    """Calculates sum_{i=1}^{n} i^2"""
     if not isinstance(n, int) or n < 1:
         return None
 
-    return sum([i**2 for i in range(1, n+1)])
+    return (n * (n + 1) * (2 * n + 1)) // 6
