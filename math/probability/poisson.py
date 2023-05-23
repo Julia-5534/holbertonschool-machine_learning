f7314636b89b3ec66dbb73f11dacb449181999ff
Julia Bullard <5534@holbertonstudents.com>
2023-05-22 22:30:49 -0500

    README, empties, test files, & Task 0

---
+++ math/probability/poisson.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Poisson Distribution"""
+
+
+class Poisson:
+    """Represents a Poisson Distribution"""
+    def __init__(self, data=None, lambtha=1.):
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = self.calculate_lambtha(data)
+
+    def calculate_lambtha(self, data):
+        n = len(data)
+        total = sum(data)
+        return total / n
