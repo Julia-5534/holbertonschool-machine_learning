4a59613c1dceb49e075f9b6dcce8e60d7c6263a8
Julia Bullard <5534@holbertonstudents.com>
2023-05-22 22:41:33 -0500

    Task 1

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -5,6 +5,7 @@
 class Poisson:
     """Represents a Poisson Distribution"""
     def __init__(self, data=None, lambtha=1.):
+        """Initialize Poisson Distribution"""
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
@@ -17,6 +18,19 @@ class Poisson:
             self.lambtha = self.calculate_lambtha(data)
 
     def calculate_lambtha(self, data):
+        """Calculates lambtha of the distribution"""
         n = len(data)
         total = sum(data)
         return total / n
+
+    def pmf(self, k):
+        """Calculates value of PMF for given number of 'successes'"""
+        k = int(k)
+        if k < 0:
+            return 0
+        else:
+            e_to_the_power = -self.lambtha
+            product = 1
+            for i in range(1, k+1):
+                product *= self.lambtha / i
+            return product * pow(2.71828, e_to_the_power)
