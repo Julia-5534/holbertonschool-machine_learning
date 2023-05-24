f979b41699af80f0c7725de7ff5b6e84c676370c
Julia Bullard <5534@holbertonstudents.com>
2023-05-23 22:26:31 -0500

    Task 9 hopefully

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -22,8 +22,26 @@ class Normal:
                 ** 0.5
             )
 
+    def z_score(self, x):
+        """Calculates z-score of given x-value"""
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """Calculates x-value of given z-score"""
+        return self.mean + z * self.stddev
+
     def pdf(self, x):
         """Calculates value of PDF for given x-value"""
         coefficient = 1 / (self.stddev * (2 * 3.1415926536) ** 0.5)
         exponent = -((x - self.mean) ** 2) / (2 * self.stddev ** 2)
         return coefficient * (2.7182818285 ** exponent)
+
+    def cdf(self, x):
+        """Calculates value of CDF for given x-value"""
+        z = self.z_score(x)
+        return 0.5 * (1 + self.errorf_approx(z / 2 ** 0.5))
+
+    def errorf_approx(self, x):
+        """Approximates error function"""
+        pi = 3.1415926536
+        return (2/(pi**.5))*(x-(x**3)/3 + (x**5)/10 - (x**7)/42 + (x**9)/216)
