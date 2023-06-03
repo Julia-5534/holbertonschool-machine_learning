50b65c6713b0ad3bc700e96ce0fb264ccd55808b
Julia Bullard <5534@holbertonstudents.com>
2023-06-03 16:52:12 -0500

    Task 0 Final?

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -5,18 +5,15 @@ import numpy as np
 
 
 class Neuron:
-    """Defines a single neuron performing binary classifications"""
     def __init__(self, nx):
-        """Initializes Neuron Class"""
-        if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
-        if nx < 1:
-            raise ValueError("nx must be a positive integer")
-
-        self.W = np.random.randn(1, nx)
+        """Initialize the Neuron class"""
+        self.nx = nx
+        self.W = np.random.normal(size=(1, nx))
         self.b = 0
         self.A = 0
 
-    def __str__(self):
-        """Returns A String"""
-        return f"[[{np.array2string(self.W, separator=', ').strip('[]')}]]"
+    def forward_prop(self, X):
+        """Perform forward propagation"""
+        z = np.matmul(self.W, X) + self.b
+        self.A = 1 / (1 + np.exp(-z))
+        return self.A
