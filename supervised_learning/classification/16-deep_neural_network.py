d5b33bb6f3ae18d6bf6d44b30bb416e68ab177f9
Julia Bullard <5534@holbertonstudents.com>
2023-06-04 00:00:17 -0500

    Task 16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -7,7 +7,7 @@ import numpy as np
 class DeepNeuralNetwork:
     """DeepNeuralNetwork"""
     def __init__(self, nx, layers):
-        """Initialize the DeepNeuralNetwork"""
+        """Initialize DeepNeuralNetwork"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -17,39 +17,18 @@ class DeepNeuralNetwork:
         if not all(isinstance(n, int) and n > 0 for n in layers):
             raise TypeError("layers must be a list of positive integers")
 
-        self.__L = len(layers)
-        self.__cache = {}
-        self.__weights = {}
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
 
-        for l_ind in range(1, self.__L + 1):
-            if isinstance(layers[l_ind - 1], int) and layers[l_ind - 1] > 0:
-                key_w = 'W' + str(l_ind)
-                key_b = 'b' + str(l_ind)
-                self.__weights[key_w] = (
-                    np.random.randn(layers[l_ind - 1], nx) *
+        for l_ind, nodes in enumerate(layers, 1):
+            if isinstance(nodes, int) and nodes > 0:
+                self.weights[f"W{l_ind}"] = (
+                    np.random.randn(nodes, nx) *
                     np.sqrt(2 / nx)
                     )
 
-                self.__weights[key_b] = np.zeros((layers[l_ind - 1], 1))
+                self.weights[f"b{l_ind}"] = np.zeros((nodes, 1))
+                nx = nodes
             else:
                 raise TypeError("layers must be a list of positive integers")
-
-    @property
-    def L(self):
-        return self.__L
-
-    @property
-    def cache(self):
-        return self.__cache
-
-    @property
-    def weights(self):
-        return self.__weights
-
-    @L.setter
-    def L(self, value):
-        if not isinstance(value, int):
-            raise TypeError("L must be an integer")
-        if value < 1:
-            raise ValueError("L must be a positive integer")
-        self.__L = value
