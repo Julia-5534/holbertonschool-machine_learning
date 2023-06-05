00ce6ddb4a9747bd9761688d2733fb59478be340
Julia Bullard <5534@holbertonstudents.com>
2023-06-05 16:17:31 -0500

    Task 16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -7,28 +7,47 @@ import numpy as np
 class DeepNeuralNetwork:
     """DeepNeuralNetwork"""
     def __init__(self, nx, layers):
-        """Initialize DeepNeuralNetwork"""
+        """Initializes DeepNeuralNetwork"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(n, int) and n > 0 for n in layers):
+        if not all(isinstance(layer, int) and layer > 0 for layer in layers):
             raise TypeError("layers must be a list of positive integers")
 
-        self.L = len(layers)
-        self.cache = {}
-        self.weights = {}
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
 
-        for l_ind, nodes in enumerate(layers, 1):
-            if isinstance(nodes, int) and nodes > 0:
-                self.weights[f"W{l_ind}"] = (
-                    np.random.randn(nodes, nx) *
-                    np.sqrt(2 / nx)
-                    )
-
-                self.weights[f"b{l_ind}"] = np.zeros((nodes, 1))
-                nx = nodes
+        for lix in range(1, self.__L + 1):
+            if lix == 1:
+                self.__weights['W' + str(lix)] = np.random.randn(layers[lix-1],
+                                                                 nx) \
+                                                     * np.sqrt(2/nx)
             else:
-                raise TypeError("layers must be a list of positive integers")
+                self.__weights['W' + str(lix)] = \
+                    np.random.randn(layers[lix-1], layers[lix-2]) \
+                    * np.sqrt(2/layers[lix-2])
+            self.__weights['b' + str(lix)] = np.zeros((layers[lix-1], 1))
+
+    @property
+    def L(self):
+        return self.__L
+
+    @L.setter
+    def L(self, value):
+        if not isinstance(value, int):
+            raise TypeError("L must be an integer")
+        if value < 1:
+            raise ValueError("L must be a positive integer")
+        self.__L = value
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights
