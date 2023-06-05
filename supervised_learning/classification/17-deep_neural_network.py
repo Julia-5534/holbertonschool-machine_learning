41fbaaec744e9e445421bf8d96aee7f0237c06a7
Julia Bullard <5534@holbertonstudents.com>
2023-06-05 16:46:10 -0500

    Task 17

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -5,14 +5,16 @@ import numpy as np
 
 
 class DeepNeuralNetwork:
+    """DeepNeuralNetwork"""
     def __init__(self, nx, layers):
+        """Initializes DeepNeuralNetwork"""
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(layers) is not list:
+        if type(layers) is not list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(layer, int) and layer > 0 for layer in layers):
+        if min(layers) <= 0:
             raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
