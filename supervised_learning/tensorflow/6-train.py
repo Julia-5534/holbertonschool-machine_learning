3b9feb3364a6ecf5170f4b80b55ae9ca56de72e7
Julia Bullard <5534@holbertonstudents.com>
2023-06-14 12:39:12 -0500

    Fixed Task 6

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -12,53 +12,39 @@ forward_prop = __import__('2-forward_prop').forward_prop
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
           activations, alpha, iterations, save_path="/tmp/model.ckpt"):
     """Train the model"""
-    tf.reset_default_graph()
-
     # Create placeholders for input data and labels
     x, y = create_placeholders(*X_train.shape[1:], *Y_train.shape[1:])
-
     # Build the forward propagation graph
     y_pred = forward_prop(x, layer_sizes, activations)
-
     # Calculate the loss and accuracy
     loss = calculate_loss(y, y_pred)
     accuracy = calculate_accuracy(y, y_pred)
-
     # Create the training operation
     train_op = create_train_op(loss, alpha)
-
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
+    tf.add_to_collection('y_pred', y_pred)
+    tf.add_to_collection('loss', loss)
+    tf.add_to_collection('accuracy', accuracy)
+    tf.add_to_collection('train_op', train_op)
     # Initialize variables
     init = tf.global_variables_initializer()
-
-    # Create a saver object to save the model
+    sess = tf.Session()
+    sess.run(init)
+    for i in range(iterations + 1):
+        if i % 100 == 0 or i == iterations:
+            train_cost, train_acc = sess.run((loss, accuracy),
+                                             feed_dict={x: X_train,
+                                                        y: Y_train})
+            valid_cost, valid_acc = sess.run((loss, accuracy),
+                                             feed_dict={x: X_valid,
+                                                        y: Y_valid})
+            print("After {} iterations:".format(i))
+            print("\tTraining Cost: {}".format(train_cost))
+            print("\tTraining Accuracy: {}".format(train_acc))
+            print("\tValidation Cost: {}".format(valid_cost))
+            print("\tValidation Accuracy: {}".format(valid_acc))
+        if i < iterations:
+            sess.run(train_op, feed_dict={x: X_train, y: Y_train})
     saver = tf.train.Saver()
-
-    with tf.Session() as sess:
-        sess.run(init)
-
-        # Training loop
-        for i in range(iterations + 1):
-            # Perform one training iteration
-            _, cost = sess.run([train_op, loss],
-                               feed_dict={x: X_train, y: Y_train})
-
-            # Print progress after every 100 iterations
-            if i % 100 == 0:
-                train_acc = sess.run(accuracy,
-                                     feed_dict={x: X_train, y: Y_train})
-                valid_cost = sess.run(loss,
-                                      feed_dict={x: X_valid, y: Y_valid})
-                valid_acc = sess.run(accuracy,
-                                     feed_dict={x: X_valid, y: Y_valid})
-
-                print(f"After {i} iterations:")
-                print(f"\tTraining Cost: {cost}")
-                print(f"\tTraining Accuracy: {train_acc}")
-                print(f"\tValidation Cost: {valid_cost}")
-                print(f"\tValidation Accuracy: {valid_acc}")
-
-        # Save the model
-        save_path = saver.save(sess, save_path)
-        print(f"Model saved in path: {save_path}")
-
-    return save_path
+    return saver.save(sess, save_path)
