8eb111504ebdbbee2f48a507c5b4005d973076e7
Julia Bullard <5534@holbertonstudents.com>
2023-06-14 12:26:39 -0500

    Fixed Task 4. Task 5

--- supervised_learning/tensorflow/4-calculate_loss.py
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -6,9 +6,4 @@ import tensorflow as tf
 
 def calculate_loss(y, y_pred):
     """Calculate the softmax cross-entropy loss"""
-    labels = y
-    logits = y_pred
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels,
-                                                                  logits))
-
-    return loss
+    return tf.losses.softmax_cross_entropy(y, y_pred)
--- supervised_learning/tensorflow/5-create_train_op.py
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 """Task 5"""
+
+import tensorflow as tf
+
+
+def create_train_op(loss, alpha):
+    """Creates training operation for network using gradient descent"""
+    return tf.train.GradientDescentOptimizer(alpha).minimize(loss)
