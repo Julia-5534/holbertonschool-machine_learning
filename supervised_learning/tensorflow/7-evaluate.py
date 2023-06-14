e24424a206dab62372691eba4478f1b200b9328d
Julia Bullard <5534@holbertonstudents.com>
2023-06-14 12:42:54 -0500

    Task 7

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -1,2 +1,29 @@
 #!/usr/bin/env python3
 """Task 7"""
+
+import tensorflow as tf
+import numpy as np
+
+
+def evaluate(X, Y, save_path):
+    """Evaluates output of neural network"""
+    # Load the graph
+    graph = tf.Graph()
+    with graph.as_default():
+        saver = tf.train.import_meta_graph(save_path + '.meta')
+
+    with tf.Session(graph=graph) as sess:
+        # Restore the saved variables
+        saver.restore(sess, save_path)
+
+        # Get the tensors from the graph's collection
+        y_pred = graph.get_tensor_by_name('y_pred:0')
+        loss = graph.get_tensor_by_name('loss:0')
+        accuracy = graph.get_tensor_by_name('accuracy:0')
+
+        # Evaluate the network
+        feed_dict = {'input:0': X, 'labels:0': Y}
+        prediction, acc, l_val = sess.run([y_pred, accuracy, loss],
+                                          feed_dict=feed_dict)
+
+    return prediction, acc, l_val
