3434d43ce9fb392307d121d7101f4e2a58d638ab
Julia Bullard <5534@holbertonstudents.com>
2023-06-14 12:00:11 -0500

    Fixed Task 1

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -5,7 +5,8 @@ import tensorflow as tf
 
 
 def create_layer(prev, n, activation):
+    """Creates layers"""
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.layers.dense(prev, n, act=activation,
+    layer = tf.layers.dense(prev, n, activation=activation,
                             kernel_initializer=init)
     return layer
