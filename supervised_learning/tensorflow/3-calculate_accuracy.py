06c3cfd464fa399ec8b96929431e9ab806b6a3c9
Julia Bullard <5534@holbertonstudents.com>
2023-06-14 12:18:45 -0500

    Task 3

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 """Task 3"""
+
+import tensorflow as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """Calculate Accuracy"""
+    # Count the number of correct predictions
+    correct_predictions = tf.equal(tf.argmax(y, axis=1),
+                                   tf.argmax(y_pred, axis=1))
+
+    # Calculate accuracy as ratio of correct predictions to all predictions
+    accuracy = tf.reduce_mean(tf.cast(correct_predictions,
+                                      dtype=tf.float32))
+
+    return accuracy
