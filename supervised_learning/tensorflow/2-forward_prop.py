042a1a68752ede765f4c5a66b232766420b64bc3
Julia Bullard <5534@holbertonstudents.com>
2023-06-14 12:09:47 -0500

    Task 2

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 """Task 2"""
+
+import tensorflow as tf
+create_layer = __import__('1-create_layer').create_layer
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """Perform forward propagation through each layer"""
+    for i in range(len(layer_sizes)):
+        # Create a layer with the specified size and activation function
+        outtie = create_layer(x, layer_sizes[i], activations[i])
+
+    return outtie
