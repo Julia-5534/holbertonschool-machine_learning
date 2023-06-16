6a0c46721fc09c0758ec2c7a56f80021a2337dc1
Julia Bullard <5534@holbertonstudents.com>
2023-06-16 12:22:45 -0500

    Task 2

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -3,15 +3,16 @@
 
 import numpy as np
 
+
 def shuffle_data(X, Y):
     """Shuffles data points in 2 matrices the same way"""
     assert X.shape[0] == Y.shape[0]
-    
+
     # Generate a random permutation
     perm = np.random.permutation(X.shape[0])
-    
+
     # Shuffle the matrices based on the permutation
     shuffled_X = X[perm]
     shuffled_Y = Y[perm]
-    
+
     return shuffled_X, shuffled_Y
