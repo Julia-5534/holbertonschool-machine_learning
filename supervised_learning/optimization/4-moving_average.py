c7e80436581f17d8e08efdc53b0452ab6733fdfb
Julia Bullard <5534@holbertonstudents.com>
2023-06-17 15:16:40 -0500

    Total push for sandbox.

---
+++ supervised_learning/optimization/4-moving_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Task 4"""
+
+def moving_average(data, beta):
+    moving_averages = []
+    bias_correction = 1 - beta
+
+    weighted_average = 0
+    for i, value in enumerate(data):
+        weighted_average = (beta * weighted_average) + ((1 - beta) * value)
+        bias_corrected_average = weighted_average / bias_correction
+        moving_averages.append(bias_corrected_average)
+
+    return moving_averages
