ace918224f42312376c4db10221cef7c4b673811
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 13:38:28 -0500

    Task 3

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -9,6 +9,6 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     regularizer = tf.contrib.layers.l2_regularizer(lambtha)
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     layer = tf.layers.Dense(
-        units=n, activation=activation,
+        n, activation=activation,
         kernel_regularizer=regularizer, kernal_initializer=init)
     return layer(prev)
