2ca324bdd60df086a840710828178b4b069658fe
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 15:31:49 -0500

    Task 5 Documented

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -6,7 +6,8 @@ import tensorflow.keras as K
 
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
-
+    """Trains model using mini-batch gradient descent
+    and also analyzes validation data"""
     history = network.fit(data, labels, batch_size=batch_size, epochs=epochs,
                           validation_data=validation_data,
                           verbose=verbose, shuffle=shuffle)
