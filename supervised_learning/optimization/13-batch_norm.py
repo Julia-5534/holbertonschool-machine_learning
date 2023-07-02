6301400dad683984044ca02b9ce0d1c6f1b0c0a5
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 09:51:38 -0500

    Task 13

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
 """Task 13"""
+
+import numpy as np
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """Normalizes unactivated output of
+    neural network using batch normalization"""
+    # Calculate the mean and variance of Z
+    mean = np.mean(Z, axis=0)
+    variance = np.var(Z, axis=0)
+
+    # Normalize Z
+    Z_norm = (Z - mean) / np.sqrt(variance + epsilon)
+
+    # Scale and shift the normalized Z
+    Z_scaled = gamma * Z_norm + beta
+
+    return Z_scaled
