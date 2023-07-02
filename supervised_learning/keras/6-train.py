3bed88815d1e9ebe3baa10d2537acf27dd65e930
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 15:44:21 -0500

    Task 6

---
+++ supervised_learning/keras/6-train.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Task 6"""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, verbose=True, shuffle=False):
+    """Trains model same as 6-train
+    PLUS trains using early stopping"""
+    callbacks = []
+    if early_stopping and validation_data is not None:
+        early_stop_callback = K.callbacks.EarlyStopping(
+            monitor='val_loss', patience=patience)
+        callbacks.append(early_stop_callback)
+
+    history = network.fit(data, labels, batch_size=batch_size, epochs=epochs,
+                          validation_data=validation_data,
+                          callbacks=callbacks,
+                          verbose=verbose, shuffle=shuffle)
+    return history
