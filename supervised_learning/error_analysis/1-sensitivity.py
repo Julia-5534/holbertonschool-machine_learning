4fd21e31224e9c4dbffb13e31a549a2c758a7172
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 12:29:22 -0500

    Task 1

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -7,12 +7,11 @@ import numpy as np
 def sensitivity(confusion):
     """Calculates the sensitivity for each class in a confusion matrix"""
     classes = confusion.shape[0]
-    sensitivity_values = np.zeros(classes, dtype=np.float32)
+    sensitivity_values = np.zeros(classes)
 
     for i in range(classes):
         true_positives = confusion[i, i]
-        false_negatives = np.sum(confusion[i, :]) - true_positives
-        sensitivity_values[i] = true_positives / (
-            true_positives + false_negatives)
+        false_negatives = np.sum(confusion[i, :])
+        sensitivity_values[i] = true_positives / false_negatives
 
     return sensitivity_values
