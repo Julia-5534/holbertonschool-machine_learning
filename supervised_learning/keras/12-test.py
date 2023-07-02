4bde7f00e9c12c03dadc7e9bc295c16e423961ae
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 16:12:40 -0500

    Task 12

---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Task 12"""
+
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """Tests a neural network"""
+    loss, accuracy = network.evaluate(data, labels, verbose=verbose)
+    return loss, accuracy
