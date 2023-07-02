d56c327ed06379a3d768ce840c8493e9b5a8915d
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 13:27:50 -0500

    Simplified Task 2

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -7,6 +7,4 @@ import tensorflow as tf
 def l2_reg_cost(cost):
     """Calculates the cost of neural
     network with L2 regularization"""
-    l2_loss = tf.losses.get_regularization_loss()
-    l2_cost = cost + l2_loss
-    return l2_cost
+    return (cost + tf.losses.get_regularization_losses())
