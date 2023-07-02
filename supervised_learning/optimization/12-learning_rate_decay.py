876673e4a5268efe9ac39d5eb156c2c3bc64d727
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 11:40:47 -0500

    Task 12

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -5,5 +5,7 @@ import tensorflow as tf
 
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """Creates a learning rate decay operation
+    in tensorflow using inverse time decay"""
     return tf.train.inverse_time_decay(
         alpha, global_step, decay_step, decay_rate, staircase=True)
