b14f1deeee6708c367ef259680f7271343b97138
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 09:12:20 -0500

    Task 5

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 """Task 5"""
+
+import numpy as np
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """Updates variable using grad des w/ momentum optimization algorithm"""
+    v_new = beta1 * v + (1 - beta1) * grad
+    var_new = var - alpha * v_new
+    return var_new, v_new
