1961a9f939e42c8ac5e77b0fb8bcfe294297e9a1
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 15:18:43 -0500

    Task 3 Again

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -6,5 +6,4 @@ import tensorflow.keras as K
 
 def one_hot(labels, classes=None):
     """Converts a label vector into a one-hot matrix"""
-    one_hot_matrix = K.to_categorical(labels, num_classes=classes)
-    return one_hot_matrix
+    return K.utils.to_categorical(labels, classes)
