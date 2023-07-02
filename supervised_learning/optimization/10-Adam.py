cd3c1b0d10f4380b5487ca2dfb16a7bae80b9563
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 09:42:51 -0500

    Task 10

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 """Task 10"""
+
+import tensorflow as tf
+
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """Creates training op for neural network in tensorflow
+    using the Adam optimization algorithm"""
+    optimizer = tf.train.AdamOptimizer(
+        learning_rate=alpha, beta1=beta1, beta2=beta2, epsilon=epsilon)
+    train_op = optimizer.minimize(loss)
+    return train_op
