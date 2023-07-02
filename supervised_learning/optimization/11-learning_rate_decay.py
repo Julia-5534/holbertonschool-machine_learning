cd404f61485cc1d514154aab30d39bb7c5b869e8
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 09:45:46 -0500

    Task 11

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 """Task 11"""
+
+import numpy as np
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """Updates learning rate using inverse time decay in numpy"""
+    updated_alpha = alpha / (1 + decay_rate * (global_step // decay_step))
+    return updated_alpha
