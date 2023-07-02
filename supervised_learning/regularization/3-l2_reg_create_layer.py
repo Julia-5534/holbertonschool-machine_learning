5638ae3d8787f4d5f7f31388c4fbffa10452cbf1
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 13:54:42 -0500

    Task 3 Again

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -5,12 +5,24 @@ import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """Creates a tensorflow layer that includes L2 regularization"""
-    reggie = tf.contrib.layers.l2_regularizer(lambtha)
-    initi = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    """
+    Function to create a tf layer with L2 regularization
+    Args:
+        prev: tensor containing the output of the previous layer
+        n: the number of nodes the new layer should contain
+        activation:  the activation function that should be used on the layer
+        lambtha: L2 regularization parameter
+    Returns: output of the new layer
+    """
+    layer_weights = \
+        tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+
     layer = tf.layers.Dense(
         n,
         activation=activation,
-        kernal_regularizer=reggie,
-        kernal_initializer=initi)
+        kernel_initializer=layer_weights,
+        name="layer",
+        kernel_regularizer=tf.contrib.layers.l2_regularizer(lambtha)
+    )
+
     return layer(prev)
