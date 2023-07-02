02e3923c2a0ee5624a5633278e44506543e490e9
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 09:07:36 -0500

    Task 4

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -3,13 +3,13 @@
 
 
 def moving_average(data, beta):
-    """Calulates weighted moving average of a dataset"""
+    """Calculates weighted moving average of a dataset"""
     moving_averages = []
     avg = 0
-    bias_correction = avg / (1 - beta ** (i + 1))
 
     for i in range(len(data)):
         avg = beta * avg + (1 - beta) * data[i]
+        bias_correction = avg / (1 - beta ** (i + 1))
         moving_averages.append(bias_correction)
 
     return moving_averages
