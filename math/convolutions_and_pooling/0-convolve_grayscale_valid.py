df5a52d717586c69506cfc8f6c3127098737ea45
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 21:41:45 -0500

    Task 0

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -9,25 +9,17 @@ def convolve_grayscale_valid(images, kernel):
     m, h, w = images.shape
     kh, kw = kernel.shape
 
-    # Calculate output dimensions
+    # Calculate the output dimensions
     oh = h - kh + 1
     ow = w - kw + 1
 
-    # Create an empty array for the convolved images
-    images_conv = np.zeros((m, oh, ow))
+    # Create an empty array to store the convolved images
+    convolved_images = np.empty((m, oh, ow))
 
-    # Iterate over each image
-    for i in range(m):
-        # Iterate over each pixel position in the image
-        for j in range(oh):
-            for k in range(ow):
-                # Extract the patch from the image
-                patch = images[i, j:j+kh, k:k+kw]
-                # Perform element-wise multiplication of patch & kernel
-                convolved_patch = np.multiply(patch, kernel)
-                # Calculate the sum of the convolved patch
-                convolved_value = np.sum(convolved_patch)
-                # Assign the convolved value to the output image
-                images_conv[i, j, k] = convolved_value
+    # Perform the convolution
+    for i in range(oh):
+        for j in range(ow):
+            convolved_images[:, i, j] = np.sum(
+                images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
 
-    return images_conv
+    return convolved_images
