456bbac32b2a1d3a70fd2a46acd0d1279a9fd626
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 21:45:38 -0500

    Task 1

---
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Task 1"""
+
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    """Performs a same convolution on grayscale images"""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+
+    # Calculate the padding sizes
+    ph = (kh - 1) // 2
+    pw = (kw - 1) // 2
+
+    # Pad the images with zeros
+    padded_images = np.pad(images,
+                           ((0, 0), (ph, ph), (pw, pw)),
+                           mode='constant')
+
+    # Calculate the output dimensions
+    oh = h
+    ow = w
+
+    # Create an empty array to store the convolved images
+    convolved_images = np.empty((m, oh, ow))
+
+    # Perform the convolution
+    for i in range(oh):
+        for j in range(ow):
+            convolved_images[:, i, j] = np.sum(
+                padded_images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
+
+    return convolved_images
