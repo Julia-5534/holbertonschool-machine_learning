c48fb6688223d67a2e6cc71b3c57e23cb04da6c6
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 22:39:44 -0500

    Task 3

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -11,15 +11,13 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     sh, sw = stride
 
     if padding == 'same':
-        pad_h = int(np.ceil((h - 1) / sh))
-        pad_w = int(np.ceil((w - 1) / sw))
-        padding = (pad_h, pad_w)
+        pad_h = int((((h - 1) * sh + kh - h) / 2) + 1)
+        pad_w = int((((w - 1) * sw + kw - w) / 2) + 1)
     elif padding == 'valid':
-        padding = (0, 0)
+        pad_h, pad_w = 0, 0
     else:
-        padding = padding
+        pad_h, pad_w = padding
 
-    pad_h, pad_w = padding
     padded_images = np.pad(images,
                            ((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
                            mode='constant')
@@ -27,11 +25,11 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     output_h = int((h + (2 * pad_h) - kh) / sh) + 1
     output_w = int((w + (2 * pad_w) - kw) / sw) + 1
     output = np.zeros((m, output_h, output_w))
+    image = np.arange(m)
 
-    for i in range(0, output_h):
-        for j in range(0, output_w):
-            img_region = padded_images[:, i*sh:i*sh+kh, j*sw:j*sw+kw]
-            conv_result = np.sum(img_region * kernel, axis=(1, 2))
-            output[:, i, j] = conv_result
+    for i in range(output_h):
+        for j in range(output_w):
+            img_region = padded_images[image, i*sh:i*sh+kh, j*sw:j*sw+kw]
+            output[image, i, j] = np.sum(img_region * kernel, axis=(1, 2))
 
     return output
