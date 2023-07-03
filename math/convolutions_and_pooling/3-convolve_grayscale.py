af3e9bc8c4ecd41407e76e5550487e154d435753
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 22:32:01 -0500

    Task 3

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -11,33 +11,26 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     sh, sw = stride
 
     if padding == 'same':
-        pad_h = int(np.ceil((h - kh + 1) / sh))
-        pad_w = int(np.ceil((w - kw + 1) / sw))
-        pad_top = int(np.floor((pad_h - 1) / 2))
-        pad_bottom = pad_h - 1 - pad_top
-        pad_left = int(np.floor((pad_w - 1) / 2))
-        pad_right = pad_w - 1 - pad_left
+        pad_h = int(np.ceil((h - 1) / sh))
+        pad_w = int(np.ceil((w - 1) / sw))
+        padding = (pad_h, pad_w)
     elif padding == 'valid':
-        pad_top = 0
-        pad_bottom = 0
-        pad_left = 0
-        pad_right = 0
+        padding = (0, 0)
     else:
-        pad_top, pad_bottom, pad_left, pad_right = padding
+        padding = padding
 
-    padded_images = np.pad(images, ((0, 0),
-                                    (pad_top, pad_bottom),
-                                    (pad_left, pad_right)),
+    pad_h, pad_w = padding
+    padded_images = np.pad(images,
+                           ((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
                            mode='constant')
 
-    output_h = int((h - kh + pad_top + pad_bottom) / sh) + 1
-    output_w = int((w - kw + pad_left + pad_right) / sw) + 1
+    output_h = int((h + (2 * pad_h) - kh) / sh) + 1
+    output_w = int((w + (2 * pad_w) - kw) / sw) + 1
     output = np.zeros((m, output_h, output_w))
 
-    for i in range(output_h):
-        for j in range(output_w):
-            img_region = padded_images[
-                :, i * sh:i * sh + kh, j * sw:j * sw + kw]
+    for i in range(0, output_h):
+        for j in range(0, output_w):
+            img_region = padded_images[:, i*sh:i*sh+kh, j*sw:j*sw+kw]
             conv_result = np.sum(img_region * kernel, axis=(1, 2))
             output[:, i, j] = conv_result
 
