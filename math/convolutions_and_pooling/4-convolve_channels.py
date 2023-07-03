f877f1cb5fa0894ada0aec66eeb47ef57715408c
Julia Bullard <5534@holbertonstudents.com>
2023-07-02 23:10:34 -0500

    Task 4 Retry

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -10,30 +10,28 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     kh, kw, _ = kernel.shape
     sh, sw = stride
 
-    if padding == 'same':
-        ph = int(((h - 1) * sh - h + kh) / 2)
-        pw = int(((w - 1) * sw - w + kw) / 2)
-    elif padding == 'valid':
+    if padding == "valid":
         ph, pw = 0, 0
+    elif padding == "same":
+        ph = int((((h - 1) * sh + kh - h) / 2) + 1)
+        pw = int((((w - 1) * sw + kw - w) / 2) + 1)
     else:
         ph, pw = padding
 
     padded_images = np.pad(images,
-                           ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                           [(0, 0), (ph, ph), (pw, pw), (0, 0)],
                            mode='constant')
-    conv_h = int((h - kh + 2 * ph) / sh) + 1
-    conv_w = int((w - kw + 2 * pw) / sw) + 1
+    conv_h = int((h - kh + 2 * ph) / sh + 1)
+    conv_w = int((w - kw + 2 * pw) / sw + 1)
 
-    convolved_images = np.zeros((m, conv_h, conv_w))
+    convolved_images = np.zeros(shape=(m, conv_h, conv_w))
 
     for i in range(conv_h):
         for j in range(conv_w):
-            i_start = i * sh
-            i_end = i_start + kh
-            j_start = j * sw
-            j_end = j_start + kw
-            image_patches = padded_images[:, i_start:i_end, j_start:j_end, :]
-            convolved_images[:, i, j] = np.sum(np.multiply(
-                image_patches, kernel), axis=(1, 2, 3))
+            convolved_images[:, i, j] = np.sum(
+                (kernel * padded_images[:, i *
+                 sh:i * sh + kh, j * sw:j * sw + kw]),
+                axis=(1, 2, 3)
+            )
 
     return convolved_images
