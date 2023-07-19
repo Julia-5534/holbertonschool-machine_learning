6e79057efbb838c0288550e4446f056df512bc3e
Julia Bullard <5534@holbertonstudents.com>
2023-07-19 16:03:50 -0500

    Task 1

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -6,70 +6,37 @@ inception_block = __import__('0-inception_block').inception_block
 
 
 def inception_network():
-    """Builds the inception network as described in
-    Going Deeper with Convolutions (2014)"""
-    input_data = Input(shape=(224, 224, 3))
-
-    # First Convolutional layer
-    x = Conv2D(
-        64, kernel_size=(7, 7),
-        strides=(2, 2), padding='same',
-        activation='relu')(input_data)
-    x = MaxPooling2D(
-        pool_size=(3, 3),
-        strides=(2, 2), padding='same')(x)
-
-    # Second Convolutional layer
-    x = Conv2D(
-        64, kernel_size=(1, 1),
-        strides=(1, 1), padding='same',
-        activation='relu')(x)
-    x = Conv2D(
-        192, kernel_size=(3, 3),
-        strides=(1, 1), padding='same',
-        activation='relu')(x)
-    x = MaxPooling2D(
-        pool_size=(3, 3), strides=(2, 2),
-        padding='same')(x)
-
-    # First Inception Block
-    x = inception_block(x, [64, 128, 32, 32])
-
-    # Second Inception Block
-    x = inception_block(x, [128, 192, 96, 64])
-
-    # MaxPooling layer
-    x = MaxPooling2D(
-        pool_size=(3, 3), strides=(2, 2),
-        padding='same')(x)
-
-    # Third Inception Block
-    x = inception_block(x, [192, 208, 48, 64])
-
-    # Fourth Inception Block
-    x = inception_block(x, [160, 224, 64, 64])
-
-    # Fifth Inception Block
-    x = inception_block(x, [128, 256, 64, 64])
-
-    # MaxPooling layer
-    x = MaxPooling2D(
-        pool_size=(3, 3), strides=(2, 2),
-        padding='same')(x)
-
-    # Sixth Inception Block
-    x = inception_block(x, [256, 320, 128, 128])
-
-    # Seventh Inception Block
-    x = inception_block(x, [384, 384, 128, 128])
-
-    # Global Average Pooling
-    x = K.layers.GlobalAveragePooling2D()(x)
-
-    # Dropout and Fully Connected layer
-    x = Dropout(0.4)(x)
-    output = Dense(units=1000, activation='softmax')(x)
-
-    model = K.models.Model(inputs=input_data, outputs=output)
-
+    """
+    Builds the inception network as described in
+    Going Deeper with Convolutions (2014)
+    :return: the keras model
+    """
+    X_input = K.Input(shape=(224, 224, 3))
+    X = K.layers.Conv2D(64, (7, 7),
+                        strides=(2, 2), padding='same',
+                        activation='relu')(X_input)
+    X = K.layers.MaxPooling2D((3, 3),
+                              strides=(2, 2), padding='same')(X)
+    X = K.layers.Conv2D(64, (1, 1),
+                        strides=(1, 1), padding='same',
+                        activation='relu')(X)
+    X = K.layers.Conv2D(192, (3, 3),
+                        strides=(1, 1), padding='same',
+                        activation='relu')(X)
+    X = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(X)
+    X = inception_block(X, [64, 96, 128, 16, 32, 32])
+    X = inception_block(X, [128, 128, 192, 32, 96, 64])
+    X = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(X)
+    X = inception_block(X, [192, 96, 208, 16, 48, 64])
+    X = inception_block(X, [160, 112, 224, 24, 64, 64])
+    X = inception_block(X, [128, 128, 256, 24, 64, 64])
+    X = inception_block(X, [112, 144, 288, 32, 64, 64])
+    X = inception_block(X, [256, 160, 320, 32, 128, 128])
+    X = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(X)
+    X = inception_block(X, [256, 160, 320, 32, 128, 128])
+    X = inception_block(X, [384, 192, 384, 48, 128, 128])
+    X = K.layers.AveragePooling2D((7, 7))(X)
+    X = K.layers.Dropout(0.4)(X)
+    X = K.layers.Dense(1000)(X)
+    model = K.models.Model(inputs=X_input, outputs=X)
     return model
