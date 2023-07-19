f665eb1d57e80c7ac0a02d01fae890db1b1ebabd
Julia Bullard <5534@holbertonstudents.com>
2023-07-19 16:32:25 -0500

    Task 2

---
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""Task 2"""
+
+import tensorflow.keras as K
+
+
+def identity_block(A_prev, filters):
+    """
+    Implementation of the identity block as defined in
+    Deep Residual Learning for Image Recognition (2015)
+
+    Arguments:
+    A_prev -- output tensor of previous layer,
+    of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    filters -- tuple or list containing F11, F3, F12, respectively:
+                F11 -- number of filters in the first 1x1 convolution
+                F3 -- number of filters in the 3x3 convolution
+                F12 -- number of filters in the second 1x1 convolution
+
+    Returns:
+    X -- output of the identity block, tensor of shape (n_H, n_W, n_C)
+    """
+
+    # Retrieve Filters
+    F11, F3, F12 = filters
+
+    # Save the input value
+    X_shortcut = A_prev
+
+    # First component of main path
+    X = K.layers.Conv2D(
+        filters=F11, kernel_size=(1, 1),
+        strides=(1, 1), padding='valid',
+        kernel_initializer=K.initializers.he_normal(seed=0))(A_prev)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+
+    # Second component of main path
+    X = K.layers.Conv2D(
+        filters=F3, kernel_size=(3, 3),
+        strides=(1, 1), padding='same',
+        kernel_initializer=K.initializers.he_normal(seed=0))(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+
+    # Third component of main path
+    X = K.layers.Conv2D(
+        filters=F12, kernel_size=(1, 1),
+        strides=(1, 1), padding='valid',
+        kernel_initializer=K.initializers.he_normal(seed=0))(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+
+    # Add shortcut value to main path & pass it through a RELU activation
+    X = K.layers.Add()([X_shortcut, X])
+    X = K.layers.Activation('relu')(X)
+
+    return X
