9e6a3f2167e29abadfcdea6238c5d31b3932508c
Julia Bullard <5534@holbertonstudents.com>
2023-07-19 18:28:51 -0500

    Task 7 Again

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -9,50 +9,60 @@ transition_layer = __import__('6-transition_layer').transition_layer
 def densenet121(growth_rate=32, compression=1.0):
     """Builds the DenseNet-121 architecture as described in
     Densely Connected Convolutional Networks"""
-    # Input layer
-    inputs = K.Input(shape=(224, 224, 3))
-    nb_filters = 64
 
-    # Initial Convolution (7x7, strides=2)
-    X = K.layers.Conv2D(filters=nb_filters,
-                        kernel_size=(7, 7),
-                        strides=(2, 2),
-                        padding='same',
-                        kernel_initializer='he_normal')(inputs)
-    X = K.layers.BatchNormalization()(X)
+    input_shape = (224, 224, 3)
+    nb_filters = 2 * growth_rate
+
+    # Input layer
+    X_input = K.Input(input_shape)
+
+    # Initial convolution layer
+    X = K.layers.BatchNormalization()(X_input)
     X = K.layers.Activation('relu')(X)
+    X = K.layers.Conv2D(
+        filters=nb_filters,
+        kernel_size=(7, 7),
+        strides=(2, 2),
+        padding='same',
+        kernel_initializer='he_normal')(X)
+
+    # Initial pooling layer
     X = K.layers.MaxPooling2D(pool_size=(3, 3),
                               strides=(2, 2),
                               padding='same')(X)
 
-    # Dense Block 1
+    # Dense block 1
     X, nb_filters = dense_block(X, nb_filters, growth_rate, 6)
 
-    # Transition Layer 1
+    # Transition layer 1
     X, nb_filters = transition_layer(X, nb_filters, compression)
 
-    # Dense Block 2
+    # Dense block 2
     X, nb_filters = dense_block(X, nb_filters, growth_rate, 12)
 
-    # Transition Layer 2
+    # Transition layer 2
     X, nb_filters = transition_layer(X, nb_filters, compression)
 
-    # Dense Block 3
+    # Dense block 3
     X, nb_filters = dense_block(X, nb_filters, growth_rate, 24)
 
-    # Transition Layer 3
+    # Transition layer 3
     X, nb_filters = transition_layer(X, nb_filters, compression)
 
-    # Dense Block 4
+    # Dense block 4
     X, nb_filters = dense_block(X, nb_filters, growth_rate, 16)
 
-    # Global Average Pooling
+    # Final batch normalization and activation
+    X = K.layers.BatchNormalization()(X)
+    X = K.layers.Activation('relu')(X)
+
+    # Global average pooling and output layer
     X = K.layers.GlobalAveragePooling2D()(X)
+    X_output = K.layers.Dense(units=1000,
+                              activation='softmax',
+                              kernel_initializer='he_normal')(X)
 
-    # Fully Connected Layer
-    outputs = K.layers.Dense(units=1000, activation='softmax')(X)
-
-    # Create the model
-    model = K.models.Model(inputs=inputs, outputs=outputs)
+    model = K.Model(inputs=X_input,
+                    outputs=X_output)
 
     return model
