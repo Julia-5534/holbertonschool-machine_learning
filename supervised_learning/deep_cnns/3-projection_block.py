042e006d58cd05cea26e6082e2cfc451a282d9a5
Julia Bullard <5534@holbertonstudents.com>
2023-07-19 17:03:36 -0500

    Task 3

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -9,36 +9,38 @@ def projection_block(A_prev, filters, s=2):
     Deep Residual Learning for Image Recognition (2015)"""
     F11, F3, F12 = filters
 
-    # Shortcut path
-    shortcut = Conv2D(
-        F12, kernel_size=(1, 1),
-        strides=(s, s), padding='valid',
-        kernel_initializer='he_normal')(A_prev)
-    shortcut = BatchNormalization(axis=3)(shortcut)
+    X_shortcut = A_prev
 
     # Main path
-    x = Conv2D(
+    X = K.layers.Conv2D(
         F11, kernel_size=(1, 1),
         strides=(s, s), padding='valid',
         kernel_initializer='he_normal')(A_prev)
-    x = BatchNormalization(axis=3)(x)
-    x = Activation('relu')(x)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
 
-    x = Conv2D(
+    X = K.layers.Conv2D(
         F3, kernel_size=(3, 3),
         strides=(1, 1), padding='same',
-        kernel_initializer='he_normal')(x)
-    x = BatchNormalization(axis=3)(x)
-    x = Activation('relu')(x)
+        kernel_initializer='he_normal')(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
 
-    x = Conv2D(
+    X = K.layers.Conv2D(
         F12, kernel_size=(1, 1),
         strides=(1, 1), padding='valid',
-        kernel_initializer='he_normal')(x)
-    x = BatchNormalization(axis=3)(x)
+        kernel_initializer='he_normal')(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+
+    # Shortcut path
+    X_shortcut = K.layers.Conv2D(
+        F12, kernel_size=(1, 1),
+        strides=(s, s), padding='valid',
+        kernel_initializer='he_normal')(A_prev)
+    X_shortcut = K.layers.BatchNormalization(axis=3)(X_shortcut)
 
     # Add the main and shortcut paths
-    x = Add()([x, shortcut])
-    x = Activation('relu')(x)
+    X = K.layers.Add()([X, X_shortcut])
+    X = K.layers.Activation('relu')(X)
 
-    return x
+    return X
