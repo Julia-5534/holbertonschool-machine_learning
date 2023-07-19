d958e605c89a9d34a39fe3712eea89d3f22234b4
Julia Bullard <5534@holbertonstudents.com>
2023-07-19 17:18:11 -0500

    Task 6

---
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Task 6"""
+
+import tensorflow.keras as K
+
+
+def dense_block(X, nb_filters, growth_rate, layers):
+    for i in range(layers):
+        # Bottleneck layer (1x1 Convolution)
+        X_bottleneck = K.layers.BatchNormalization()(X)
+        X_bottleneck = K.layers.Activation('relu')(X_bottleneck)
+        X_bottleneck = K.layers.Conv2D(
+            filters=4 * growth_rate,
+            kernel_size=(1, 1),
+            padding='same',
+            kernel_initializer='he_normal')(X_bottleneck)
+
+        # Convolution layer (3x3 Convolution)
+        X_conv = K.layers.BatchNormalization()(X_bottleneck)
+        X_conv = K.layers.Activation('relu')(X_conv)
+        X_conv = K.layers.Conv2D(
+            filters=growth_rate,
+            kernel_size=(3, 3),
+            padding='same',
+            kernel_initializer='he_normal')(X_conv)
+
+        # Concatenate the output of each layer with the input X
+        X = K.layers.concatenate([X, X_conv])
+        nb_filters += growth_rate
+
+    return X, nb_filters
