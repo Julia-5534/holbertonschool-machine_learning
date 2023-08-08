040ed4124d8971d7d720ba396b53372851be0461
Julia Bullard <5534@holbertonstudents.com>
2023-08-08 16:03:35 -0500

    Task 1?

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -2,9 +2,11 @@
 """Task 1"""
 
 import numpy as np
+from tensorflow import keras as K
 
 
 class Yolo:
+    """Yolo Class"""
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """
         model_path: path to where a Darknet Keras model is stored
@@ -19,7 +21,7 @@ class Yolo:
             anchor_boxes: number of anchor boxes used for each prediction
             2 => [anchor_box_width, anchor_box_height]
         """
-        self.model = keras.models.load_model(model_path)
+        self.model = K.models.load_model(model_path)
         with open(classes_path, 'r') as f:
             self.class_names = f.read().splitlines()
         self.class_t = class_t
@@ -27,105 +29,54 @@ class Yolo:
         self.anchors = anchors
 
     def process_outputs(self, outputs, image_size):
-        """
-        outputs: list of numpy.ndarrays containing the predictions from
-        the Darknet model for a single image:
-            Each output will have the shape
-            (grid_height, grid_width, anchor_boxes, 4 + 1 + classes)
-            grid_height & grid_width => height & width of grid used for output
-            anchor_boxes => the number of anchor boxes used
-            4 => (t_x, t_y, t_w, t_h)
-            1 => box_confidence
-            classes => class probabilities for all classes
-        image_size: numpy.ndarray containing the image’s
-        original size [image_height, image_width]
-        Returns a tuple of (boxes, box_confidences, box_class_probs):
-            boxes: list of numpy.ndarrays of shape
-            (grid_height, grid_width, anchor_boxes, 4)
-            containing the processed boundary boxes for
-            each output, respectively:
-                4 => (x1, y1, x2, y2)
-                (x1, y1, x2, y2) should represent the boundary box
-                relative to original image
-            box_confidences: list of numpy.ndarrays of shape
-            (grid_height, grid_width, anchor_boxes, 1)
-            containing the box confidences for each output, respectively
-            box_class_probs: list of numpy.ndarrays of shape
-            (grid_height, grid_width, anchor_boxes, classes)
-            containing the box’s class probabilities for
-            each output, respectively
-        """
+        """Processes Outputs"""
         boxes = []
         box_confidences = []
         box_class_probs = []
 
-        # Process each output in outputs
-        for i in range(len(outputs)):
-            output = outputs[i]
+        img_height, img_width = image_size
 
-            # Get grid dimensions and number of classes from output shape
-            grid_height = output.shape[0]
-            grid_width = output.shape[1]
+        for output in outputs:
+            grid_height, grid_width, num_anchors, _ = output.shape
             num_classes = output.shape[3] - 5
 
-            # Get anchor boxes for this output
-            anchors = self.anchors[i]
+            # Compute the xy coordinates of the grid
+            grid_y = np.arange(grid_height).reshape(1, grid_height, 1, 1)
+            grid_x = np.arange(grid_width).reshape(1, 1, grid_width, 1)
 
-            # Initialize arrays to store processed data for this output
-            boxes_i = np.zeros((
-                grid_height * grid_width * len(anchors), 4))
-            box_confidences_i = np.zeros((
-                grid_height * grid_width * len(anchors), 1))
-            box_class_probs_i = np.zeros((
-                grid_height * grid_width * len(anchors), num_classes))
+            # Compute the anchor box dimensions
+            anchor_width = self.anchors[:, :, 0:1]
+            anchor_height = self.anchors[:, :, 1:2]
 
-            # Process each cell in the grid
-            for row in range(grid_height):
-                for col in range(grid_width):
-                    # Get raw data from cell
-                    raw_box_data = output[row][col][:, :4]
-                    raw_box_confidence = output[row][col][:, [4]]
-                    raw_box_class_probs = output[row][col][:, -num_classes:]
+            # Compute the bounding box parameters
+            t_x = output[..., 0]
+            t_y = output[..., 1]
+            t_w = output[..., 2]
+            t_h = output[..., 3]
 
-                    # Calculate processed data from raw data & store in arrays
-                    boxes_i[
-                        row*grid_width*len(anchors)+col*len(anchors):(
-                            row*grid_width*len(anchors)+col*len(anchors)+len(
-                                anchors)), :] = self._process_box_data(
-                                    raw_box_data)
-                    box_confidences_i[
-                        row*grid_width*len(anchors)+col*len(anchors):(
-                            row*grid_width*len(anchors)+col*len(anchors)+len(
-                                anchors)), :] = raw_box_confidence
-                    box_class_probs_i[
-                        row*grid_width*len(anchors)+col*len(anchors):(
-                            row*grid_width*len(anchors)+col*len(anchors)+len(
-                                anchors)), :] = raw_box_class_probs
+            # Convert the bounding box parameters to actual coordinates
+            pred_boxes_x = (t_x + grid_x) / grid_width
+            pred_boxes_y = (t_y + grid_y) / grid_height
+            pred_boxes_w = anchor_width * np.exp(t_w)
+            pred_boxes_h = anchor_height * np.exp(t_h)
 
-            # Reshape arrays to desired shape and append to lists
-            boxes.append(
-                np.reshape(boxes_i, (
-                    grid_height, grid_width, len(anchors), 4)))
-            box_confidences.append(
-                np.reshape(box_confidences_i, (
-                    grid_height, grid_width, len(anchors), 1)))
-            box_class_probs.append(
-                np.reshape(box_class_probs_i, (
-                    grid_height, grid_width, len(anchors), num_classes)))
+            # Normalize the box coordinates to the image size
+            pred_boxes_x *= img_width
+            pred_boxes_y *= img_height
+            pred_boxes_w *= img_width
+            pred_boxes_h *= img_height
 
-        return (boxes, box_confidences, box_class_probs)
+            # Compute box confidence and class probabilities
+            box_confidence = 1 / (1 + np.exp(-output[..., 4:5]))
+            box_class_probabilities = 1 / (1 + np.exp(-output[..., 5:]))
 
-    def _process_box_data(self, raw_box_data):
-        """
-        Helper method to process raw box data from a single cell in the grid
-        raw_box_data: numpy.ndarray of shape (anchor_boxes, 4) containing the
-        raw box data for a single cell in the grid
-            4 => (t_x, t_y, t_w, t_h)
-        Returns a numpy.ndarray of shape (anchor_boxes, 4) containing the
-        processed box data for the cell
-            4 => (x1, y1, x2, y2)
-            (x1, y1, x2, y2) should represent the boundary box relative
-            to original image
-        """
-        # TODO: Implement this method
-        pass
+            boxes.append(np.stack([
+                pred_boxes_x,
+                pred_boxes_y,
+                pred_boxes_w,
+                pred_boxes_h],
+                axis=-1))
+            box_confidences.append(box_confidence)
+            box_class_probs.append(box_class_probabilities)
+
+        return boxes, box_confidences, box_class_probs
