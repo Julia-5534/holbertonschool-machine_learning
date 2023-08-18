91074aa1cac21651ec69a21be95780e3171e599a
Julia Bullard <5534@holbertonstudents.com>
2023-08-18 15:43:06 -0500

    Task 7 Done?

--- supervised_learning/object_detection/7-yolo.py
+++ supervised_learning/object_detection/7-yolo.py
@@ -70,8 +70,8 @@ class Yolo:
             # Normalize the coordinates
             bx /= grid_width
             by /= grid_height
-            bw /= self.model.input.shape[1].value
-            bh /= self.model.input.shape[2].value
+            bw /= self.model.input.shape[1]
+            bh /= self.model.input.shape[2]
             # Calculate the coordinates relative to the image size
             x1 = (bx - bw / 2) * image_size[1]
             y1 = (by - bh / 2) * image_size[0]
