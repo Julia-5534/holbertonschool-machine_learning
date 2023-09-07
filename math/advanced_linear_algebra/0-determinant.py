0a0872b22d734323bf67fe9a6265c08ff9a4f2c7
Julia Bullard <5534@holbertonstudents.com>
2023-09-07 16:50:25 -0500

    Task 0

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
 """Task 0"""
+
+
+def determinant(matrix):
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+    if matrix == [[]]:
+        return 1
+    n = len(matrix)
+    if any(len(row) != n for row in matrix):
+        raise ValueError("matrix must be a square matrix")
+    if n == 1:
+        return matrix[0][0]
+    if n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    det = 0
+    for i in range(n):
+        submatrix = [row[:i] + row[i+1:] for row in matrix[1:]]
+        det += (-1) ** i * matrix[0][i] * determinant(submatrix)
+    return det
