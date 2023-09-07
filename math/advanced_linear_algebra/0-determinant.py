ad008b39706eafb4df692eb68f48a511ffaf1d9b
Julia Bullard <5534@holbertonstudents.com>
2023-09-07 16:53:36 -0500

    Documentation & Pycodestyle

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -3,7 +3,9 @@
 
 
 def determinant(matrix):
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    """Calculates the Determinant of a Matrix"""
+    if not isinstance(matrix, list) or not all(
+            isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
     if matrix == [[]]:
         return 1
