c2286618d7a50d65164d2ebd31a90aeef26ee725
Julia Bullard <5534@holbertonstudents.com>
2023-09-11 06:10:42 -0500

    Task 0

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -1,2 +1,36 @@
 #!/usr/bin/env python3
 """Task 0"""
+
+import numpy as np
+
+
+def likelihood(x, n, P):
+    """Calculates the likelihood of obtaining this data given
+    various hypothetical probabilities of developing severe side effects"""
+    # Check if n is a positive integer
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    # Check if x is an integer greater than or equal to 0
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+
+    # Check if x is greater than n
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    # Check if P is a 1D numpy.ndarray
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    # Check if all values in P are in the range [0, 1]
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    # Calculate the likelihood for each probability in P
+    likelihoods = np.array([
+        np.math.comb(n, x) * (p**x) * ((
+            1 - p)**(n - x)) for p in P])
+
+    return likelihoods
