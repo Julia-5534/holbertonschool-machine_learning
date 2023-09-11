d6a0788b3040d51d03d3cb9a239a64285c4b247b
Julia Bullard <5534@holbertonstudents.com>
2023-09-11 05:27:40 -0500

    Task 0

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python3
 """Task 0"""
+
+import numpy as np
+
+
+def mean_cov(X):
+    """Calculates the mean and covariance of a dataset"""
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+
+    n, d = X.shape
+
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+
+    mean = np.mean(X, axis=0, keepdims=True)
+    cov = np.zeros((d, d))
+
+    for i in range(d):
+        for j in range(d):
+            cov[i, j] = np.sum((
+                X[:, i] - mean[0, i]) * (X[:, j] - mean[0, j])) / (n - 1)
+
+    return mean, cov
