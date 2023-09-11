5d2f120849f477800b48398e2ae3dae09fcda19c
Julia Bullard <5534@holbertonstudents.com>
2023-09-11 06:19:06 -0500

    Task 1

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -1,2 +1,98 @@
 #!/usr/bin/env python3
 """Task 1"""
+
+import numpy as np
+
+
+def comb(n, k):
+    """
+    Calculate the binomial coefficient C(n, k).
+
+    Parameters:
+    - n: Total number of items.
+    - k: Number of items to choose.
+
+    Returns:
+    - Binomial coefficient C(n, k).
+    """
+    if 0 <= k <= n:
+        return np.math.factorial(n) // (
+            np.math.factorial(k) * np.math.factorial(n - k))
+    else:
+        return 0
+
+
+def likelihood(x, n, P):
+    """Calculates the likelihood of obtaining this data given
+    various hypothetical probabilities of developing severe side effects"""
+    # Check if n is a positive integer
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    # Check if x is an integer greater than or equal to 0
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+
+    # Check if x is greater than n
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    # Check if P is a 1D numpy.ndarray
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    # Check if all values in P are in the range [0, 1]
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    # Calculate the likelihood for each probability in P
+    likelihoods = np.array([
+        np.math.comb(n, x) * (p**x) * ((
+            1 - p)**(n - x)) for p in P])
+
+    return likelihoods
+
+
+def intersection(x, n, P, Pr):
+    """Calculates the intersection of obtaining this
+    data with the various hypothetical probabilities"""
+    # Check if n is a positive integer
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    # Check if x is an integer greater than or equal to 0
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+
+    # Check if x is greater than n
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    # Check if P is a 1D numpy.ndarray
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    # Check if Pr is a numpy.ndarray with the same shape as P
+    if not isinstance(Pr, np.ndarray) or Pr.ndim != 1 or Pr.shape != P.shape:
+        raise TypeError(
+            "Pr must be a numpy.ndarray with the same shape as P")
+
+    # Check if all values in P are in the range [0, 1]
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    # Check if all values in Pr are in the range [0, 1]
+    if not np.all((Pr >= 0) & (Pr <= 1)):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+
+    # Check if Pr sums to approximately 1 (using numpy.isclose)
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+
+    # Calculate the intersection for each probability in P
+    intersection_values = P * Pr * (
+        comb(n, x) * (P**x) * ((1 - P)**(n - x)))
+
+    return intersection_values
