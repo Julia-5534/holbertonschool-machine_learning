20f47ddb91bc95c2eec834943dae17918eae3b5b
Julia Bullard <5534@holbertonstudents.com>
2023-09-11 05:42:00 -0500

    Totally crushed it, I think.

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -1,2 +1,23 @@
 #!/usr/bin/env python3
 """Task 1"""
+
+import numpy as np
+
+
+def correlation(C):
+    """Calculates a Correlation Matrix"""
+    if not isinstance(C, np.ndarray):
+        raise TypeError("C must be a numpy.ndarray")
+
+    if C.ndim != 2 or C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
+
+    d = C.shape[0]
+    correlation_matrix = np.zeros((d, d))
+
+    for i in range(d):
+        for j in range(d):
+            correlation_matrix[
+                i, j] = C[i, j] / np.sqrt(C[i, i] * C[j, j])
+
+    return correlation_matrix
--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,2 +1,43 @@
 #!/usr/bin/env python3
-"""Task 2"""
+"""Tasks 2 & 3"""
+
+import numpy as np
+
+
+class MultiNormal:
+    """Multivariate Normal Distribution"""
+    def __init__(self, data):
+        """Initializes the Class"""
+        if not isinstance(data, np.ndarray) or data.ndim != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+
+        d, n = data.shape
+
+        if n < 2:
+            raise ValueError("data must contain multiple data points")
+
+        self.mean = np.mean(data, axis=1, keepdims=True)
+        self.cov = np.zeros((d, d))
+
+        for i in range(d):
+            for j in range(d):
+                self.cov[i, j] = np.sum(
+                    (data[i] - self.mean[i]) * (
+                        data[j] - self.mean[j])) / (n - 1)
+
+    def pdf(self, x):
+        """Calculates the PDF at a data point"""
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+
+        if x.shape != self.mean.shape:
+            raise ValueError(f"x must have the shape {self.mean.shape}")
+
+        d = self.mean.shape[0]
+        det_cov = np.linalg.det(self.cov)
+        inv_cov = np.linalg.inv(self.cov)
+        x_minus_mean = x - self.mean
+        exponent = -0.5 * x_minus_mean.T @ inv_cov @ x_minus_mean
+        normalization = 1 / ((2 * np.pi) ** (d / 2) * np.sqrt(det_cov))
+
+        return normalization * np.exp(exponent)[0, 0]
