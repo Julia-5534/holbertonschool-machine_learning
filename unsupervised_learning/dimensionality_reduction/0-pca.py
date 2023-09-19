07ebfcd2b4960536317139717898ec30f72eda18
Julia Bullard <5534@holbertonstudents.com>
2023-09-19 17:36:08 -0500

    Pycodestyle fix. Deal with output later

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -8,16 +8,16 @@ def pca(X, ndim=3):
     """Placeholder"""
     # Calculate covariance matrix
     covariance_matrix = np.cov(X, rowvar=False)
-    
+
     # Compute eigenvalues & eigenvectors
     eigenvalues, eigenvectors = np.linalg.eigh(covariance_matrix)
-    
+
     # Sort eigenvalues & eigenvectors in descending order
     sorted_indices = np.argsort(eigenvalues)[::-1]
     eigenvalues = eigenvalues[sorted_indices]
     eigenvectors = eigenvectors[:, sorted_indices]
-    
+
     # Select top ndim eigenvectors
     selected_eigenvectors = eigenvectors[:, :ndim]
-    
+
     return selected_eigenvectors
