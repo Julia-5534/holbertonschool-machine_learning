190e09ec5b52cb1b69559649631281642dbf3dd0
Julia Bullard <5534@holbertonstudents.com>
2023-09-19 17:34:06 -0500

    Fixing boofed up output

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -4,28 +4,20 @@
 import numpy as np
 
 
-def pca(X, var=0.95):
+def pca(X, ndim=3):
     """Placeholder"""
     # Calculate covariance matrix
     covariance_matrix = np.cov(X, rowvar=False)
-
+    
     # Compute eigenvalues & eigenvectors
     eigenvalues, eigenvectors = np.linalg.eigh(covariance_matrix)
-
+    
     # Sort eigenvalues & eigenvectors in descending order
     sorted_indices = np.argsort(eigenvalues)[::-1]
     eigenvalues = eigenvalues[sorted_indices]
     eigenvectors = eigenvectors[:, sorted_indices]
-
-    # Calculate total variance & cumulative variance explained
-    total_variance = np.sum(eigenvalues)
-    explained_variance_ratio = eigenvalues / total_variance
-    cumulative_variance_ratio = np.cumsum(explained_variance_ratio)
-
-    # Determine number of components to keep
-    num_components = np.argmax(cumulative_variance_ratio >= var) + 1
-
-    # Select top eigenvectors
-    selected_eigenvectors = eigenvectors[:, :num_components]
-
+    
+    # Select top ndim eigenvectors
+    selected_eigenvectors = eigenvectors[:, :ndim]
+    
     return selected_eigenvectors
