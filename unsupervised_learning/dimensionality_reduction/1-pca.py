9a579bf2c866f5cfd234372754bcb1abe3b201f1
Julia Bullard <5534@holbertonstudents.com>
2023-09-27 12:37:26 -0500

    For at least a point or two:

---
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Task 1"""
+
+import numpy as np
+
+
+def pca(X, ndim):
+    """Placeholder"""
+    # Standardize data (mean centering)
+    mean = np.mean(X, axis=0)
+    X_centered = X - mean
+
+    # Calculate covariance matrix
+    cov_matrix = np.cov(X_centered, rowvar=False)
+
+    # Calculate e-vals & e-vecs of covariance matrix
+    eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
+
+    # Sort e-vals & e-vecs in descending order
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    eigenvalues = eigenvalues[sorted_indices]
+    eigenvectors = eigenvectors[:, sorted_indices]
+
+    # Flip sign of e-vals (not e-vecs)
+    eigenvalues = -eigenvalues
+
+    # Project data onto new feature space
+    transformed_data = np.dot(X_centered, eigenvectors[:, :ndim])
+
+    return transformed_data
