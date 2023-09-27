93783ff016a1eddead0642eed2e794b574b3aa6f
Julia Bullard <5534@holbertonstudents.com>
2023-09-27 16:31:03 -0500

    This Task name got me wanting to listen to Wild Child

---
+++ unsupervised_learning/clustering/6-expectation.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Task 6"""
+
+import numpy as np
+pdf = __import__('5-pdf').pdf
+
+
+def expectation(X, pi, m, S):
+    """Placeholder"""
+    if (
+        type(X) is not np.ndarray or
+        type(pi) is not np.ndarray or
+        type(m) is not np.ndarray or
+        type(S) is not np.ndarray
+    ):
+        return None, None
+
+    if (
+        len(X.shape) != 2 or
+        len(pi.shape) != 1 or
+        len(m.shape) != 2 or
+        len(S.shape) != 3
+    ):
+        return None, None
+
+    n, d = X.shape
+    k = pi.shape[0]
+
+    g = np.zeros((k, n))
+    l = 0
+
+    for i in range(k):
+        P = pi[i] * pdf(X, m[i], S[i])
+        g[i] = P
+
+    g_sum = np.sum(g, axis=0)
+    g /= g_sum
+
+    l = np.sum(np.log(g_sum))
+
+    return g, l
