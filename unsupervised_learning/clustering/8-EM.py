0bdc9f0732c9ba9f5b4a0a633a68ed0fbfd0536e
Julia Bullard <5534@holbertonstudents.com>
2023-09-27 17:26:05 -0500

    HAVE to have import numpy as np

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Task 8"""
 
+import numpy as np
 initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
 maximization = __import__('7-maximization').maximization
