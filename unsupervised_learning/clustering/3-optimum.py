246f1995d966d9f539f764baef581eec1c63bd54
Julia Bullard <5534@holbertonstudents.com>
2023-09-27 12:12:26 -0500

    Don't type None twice goober

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -7,13 +7,13 @@ import numpy as np
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """Placeholder"""
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None, None
+        return None
     if not isinstance(kmin, int) or kmin <= 0 or kmin >= X.shape[0]:
-        return None, None
+        return None
     if not isinstance(kmax, int) or kmax <= 0 or kmax >= X.shape[0]:
-        return None, None
+        return None
     if not isinstance(iterations, int) or iterations <= 0:
-        return None, None
+        return None
 
     kmeans = __import__('1-kmeans').kmeans
     variance = __import__('2-variance').variance
