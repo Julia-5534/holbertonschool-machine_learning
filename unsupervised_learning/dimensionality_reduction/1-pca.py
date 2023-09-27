20ddfaed26da5845dca17dd17efaa176a57763ed
Julia Bullard <5534@holbertonstudents.com>
2023-09-27 13:10:29 -0500

    Sometimes I be doing too much. It's easier IRL than it is in my head

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -10,24 +10,10 @@ def pca(X, ndim):
     mean = np.mean(X, axis=0)
     X_centered = X - mean
 
-    # Calculate covariance matrix
-    cov_matrix = np.cov(X_centered, rowvar=False)
-
     # Calculate e-vals & e-vecs of covariance matrix
-    eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
-
-    # Sort e-vals & e-vecs in descending order
-    sorted_indices = np.argsort(eigenvalues)[::-1]
-    eigenvalues = eigenvalues[sorted_indices]
-    eigenvectors = eigenvectors[:, sorted_indices]
-
-    # Flip sign of e-vals (not e-vecs)
-    eigenvalues = -eigenvalues
+    _, _, Vt = np.linalg.svd(X_centered)
 
     # Project data onto new feature space
-    transformed_data = np.dot(X_centered, eigenvectors[:, :ndim])
+    T = np.dot(X_centered, Vt[:ndim].T)
 
-    # Change sign of the second column
-    transformed_data[:, 1] *= -1
-
-    return transformed_data
+    return T
