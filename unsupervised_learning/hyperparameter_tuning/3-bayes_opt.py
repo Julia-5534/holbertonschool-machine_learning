f8c2a7a00b20405791a26b9c70fbd952c5bc1257
Julia-5534 <112386137+Julia-5534@users.noreply.github.com>
2023-10-15 19:53:38 -0500

    Update 3-bayes_opt.py

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
 """Task 3"""
 
-GP = __import__('2-gp').GaussianProcess
 import numpy as np
+GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
     """Bayesian Optimization Class"""
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
         """Initializes Bayesian Optimization"""
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
         self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
         self.xsi = xsi
         self.minimize = minimize
-    
+
     def acquire(self):
-        """Calculates acquire function values for all points in the search space"""
+        """Calculates acquire function values for
+        all points in the search space"""
         mu_s, sigma_s = self.gp.predict(self.X_s)
         if self.minimize:
             return mu_s - self.xsi * sigma_s
