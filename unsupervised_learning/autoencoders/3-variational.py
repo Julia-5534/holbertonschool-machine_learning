09f4bb2599d44cf18266dc09447b2744246d354a
Julia Bullard <5534@holbertonstudents.com>
2023-10-23 03:15:48 -0500

    Try this

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Task 3"""
 
-# Import necessary libraries
 import tensorflow as tf
 import tensorflow.keras as keras
 
@@ -10,39 +9,6 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     """
     Create a variational autoencoder model for dimensionality reduction
     and feature learning.
-
-    Parameters:
-    - input_dims (int): The number of features or dimensions in the model
-      input.
-    - hidden_layers (list): A list containing the number of nodes for each
-      hidden layer in the encoder, respectively. The hidden layers should
-      be reversed for the decoder.
-    - latent_dims (int): The dimensions of the latent space representation.
-
-    Returns:
-    - encoder (keras.Model): The encoder model, which should output the
-      latent representation, the mean, and the log variance, respectively.
-    - decoder (keras.Model): The decoder model.
-    - auto (keras.Model): The full autoencoder model.
-
-    This function defines and returns a variational autoencoder model, which
-    consists of an encoder and a decoder. The encoder takes the input data
-    and maps it to a lower-dimensional latent space.
-    Unlike a standard autoencoder, this encoder outputs two parameters (the
-    mean and log variance) that define a Gaussian distribution in the latent
-    space. A sample from this distribution is taken as the latent
-    representation.
-
-    The decoder maps data from the latent space back to the original input
-    space, aiming to reconstruct the input data. The encoder and decoder
-    models can be used independently, and the full autoencoder model can be
-    used for training and inference.
-
-    The autoencoder model is compiled using Adam optimization and binary
-    cross-entropy loss. All layers in the encoder and decoder use the ReLU
-    activation function, except for the mean and log variance layers in the
-    encoder, which should use None, and the last layer in the decoder, which
-    should use sigmoid.
     """
     # Input layer that takes data with 'input_dims' features
     inputs = keras.layers.Input(shape=(input_dims,))
@@ -84,7 +50,7 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     # Combine the encoder and decoder to create the full autoencoder model
     outputs = decoder(encoder(inputs)[0])
     auto = keras.Model(inputs, outputs, name="vae")
-    
+
     # Define VAE loss
     reconstruction_loss = keras.losses.binary_crossentropy(inputs, outputs)
     reconstruction_loss *= input_dims
@@ -95,8 +61,6 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
 
     # Compile autoencoder model with Adam optimization & VAE loss
     auto.add_loss(vae_loss)
-
-    # Compile autoencoder model with Adam optimization
     auto.compile(optimizer='adam')
 
     # Return the encoder, decoder, and full autoencoder models
