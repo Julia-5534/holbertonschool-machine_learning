ac9025332f0514454efa6c1d7072d6ab3efbe313
Julia Bullard <5534@holbertonstudents.com>
2023-10-23 02:12:58 -0500

    Why did a previous commit suddenly get me the output check I was missing?

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -14,9 +14,9 @@ def autoencoder(input_dims, filters, latent_dims):
                               activation='relu',
                               padding='same')(x)
       x = keras.layers.MaxPooling2D((2, 2), padding='same')(x)
-      
+
     encoder = keras.Model(input_layer, x)
-      
+
     decoder_input = keras.layers.Input(shape=latent_dims)
     x = decoder_input
 
@@ -25,7 +25,7 @@ def autoencoder(input_dims, filters, latent_dims):
                             activation='relu',
                             padding='same')(x)
     x = keras.layers.UpSampling2D((2, 2))(x)
-    
+
     x = keras.layers.Conv2D(filters[1], (3, 3),
                             activation='relu',
                             padding='same')(x)
@@ -42,11 +42,10 @@ def autoencoder(input_dims, filters, latent_dims):
                             activation='sigmoid',
                             padding='same')(x)
 
-
     # Create Decoder
     decoder = keras.Model(decoder_input, x)
 
-    #Create Autoencoder
+    # Create Autoencoder
     auto = keras.Model(input_layer, decoder(encoder(input_layer)))
 
     # Compile model
