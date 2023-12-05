dca00cf739245b55d77bbe2b0dbba671cc676648
Julia Bullard <5534@holbertonstudents.com>
2023-12-04 22:31:16 -0600

    Task 2

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -3,6 +3,7 @@
 
 from gensim.models import Word2Vec
 
+
 def word2vec_model(sentences, size=100, min_count=5,
                    window=5, negative=5, cbow=True,
                    iterations=5, seed=0, workers=1):
@@ -33,12 +34,12 @@ def word2vec_model(sentences, size=100, min_count=5,
     # Create and train the Word2Vec model
     model = Word2Vec(
         sentences,
-        size=size,
+        vector_size=size,
         min_count=min_count,
         window=window,
         negative=negative,
         sg=sg,
-        iter=iterations,
+        epochs=iterations,  # Use 'epochs' instead of 'iter'
         seed=seed,
         workers=workers
     )
