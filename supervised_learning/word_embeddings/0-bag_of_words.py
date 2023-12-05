0d181bdd75df5477c315e22754c08eb0087a8496
Julia Bullard <5534@holbertonstudents.com>
2023-12-04 22:04:50 -0600

    Task 0

---
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Task 0"""
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def bag_of_words(sentences, vocab=None):
+    """
+    Creates a bag-of-words embedding matrix.
+      - Create an instance of CountVectorizer
+      - Fit & transform the sentences
+      - Get the feature names (words)
+      - Convert the sparse matrix to a dense matrix
+    """
+
+    vectorizer = CountVectorizer(vocabulary=vocab)
+    wc_matrix = vectorizer.fit_transform(sentences)
+    features = vectorizer.get_feature_names_out()
+    embeddings = wc_matrix.toarray()
+
+    return embeddings, features
