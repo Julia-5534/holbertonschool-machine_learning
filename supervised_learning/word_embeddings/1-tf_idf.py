096db05e7b32fd918eaf0dab5f9999fdee820d37
Julia Bullard <5534@holbertonstudents.com>
2023-12-04 22:10:55 -0600

    Task 1

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
 """Task 1"""
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def tf_idf(sentences, vocab=None):
+    """
+    Create a TF-IDF embedding matrix.
+      - Create an instance of TfidfVectorizer
+      - Fit and transform the sentences
+      - Get the feature names (words)
+      - Convert the sparse matrix to a dense matrix
+    """
+
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
+    tfidf_matrix = vectorizer.fit_transform(sentences)
+    features = vectorizer.get_feature_names_out()
+    embeddings = tfidf_matrix.toarray()
+
+    return embeddings, features
