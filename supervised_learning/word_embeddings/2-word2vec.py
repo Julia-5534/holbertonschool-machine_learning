304511ce3609b666a0c38e74f35dd7118296969f
Julia Bullard <5534@holbertonstudents.com>
2023-12-06 14:30:03 -0600

    Pretty up Tasks 2, 3, & 4

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -9,29 +9,21 @@ def word2vec_model(sentences, size=100, min_count=5,
                    iterations=5, seed=0, workers=1):
     """
     Creates and trains a Gensim Word2Vec model.
-
-    Parameters:
-    - sentences: A list of sentences to be trained on.
-    - size: The dimensionality of the embedding layer.
-    - min_count: The minimum number of occurrences of
-      a word for use in training.
-    - window: The maximum distance between the current
-      and predicted word within a sentence.
-    - negative: The size of negative sampling.
-    - cbow: A boolean to determine the training type;
-      True for CBOW, False for Skip-gram.
-    - iterations: The number of iterations to train over.
-    - seed: The seed for the random number generator.
-    - workers: The number of worker threads to train the model.
-
-    Returns:
-    - The trained Word2Vec model.
+      - sentences: A list of sentences to be trained on.
+      - size: The dimensionality of the embedding layer.
+      - min_count: The minimum number of occurrences of
+        a word for use in training.
+      - window: The maximum distance between the current
+        and predicted word within a sentence.
+      - negative: The size of negative sampling.
+      - cbow: A boolean to determine the training type;
+        True for CBOW, False for Skip-gram (sg).
+      - iterations: The number of iterations to train over.
+      - seed: The seed for the random number generator.
+      - workers: The number of worker threads to train the model.
     """
-
-    # Choose the training algorithm (CBOW or Skip-gram)
     sg = 0 if cbow else 1
 
-    # Create and train the Word2Vec model
     model = Word2Vec(
         sentences,
         vector_size=size,
@@ -39,7 +31,7 @@ def word2vec_model(sentences, size=100, min_count=5,
         window=window,
         negative=negative,
         sg=sg,
-        epochs=iterations,  # Use 'epochs' instead of 'iter'
+        epochs=iterations,
         seed=seed,
         workers=workers
     )
