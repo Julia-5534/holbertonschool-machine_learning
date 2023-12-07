628eec98463c22a7e4d4924f4327fc893c8b218a
Julia Bullard <5534@holbertonstudents.com>
2023-12-06 22:10:23 -0600

    Reworked Task 2 like how I did Task 1 with helper function to make it easier on my brain

--- supervised_learning/nlp_metrics/2-cumulative_bleu.py
+++ supervised_learning/nlp_metrics/2-cumulative_bleu.py
@@ -3,52 +3,72 @@
 
 from collections import Counter
 import math
+import numpy as np
+
+
+def calculate_ngrams(tokens, n):
+    """
+    Helper function to calculate n-grams from a list of tokens.
+      - tokens (list): List of tokens (words).
+      - n (int): Size of the n-gram.
+      - list: List of n-grams.
+      - Generate n-grams using a sliding window of size 'n'.
+    """
+
+    return [tuple(tokens[i:i+n]) for i in range(len(tokens) - n + 1)]
 
 
 def cumulative_bleu(references, sentence, n):
     """
-    Calculates the Cumulative BLEU score for a given
-    sentence and reference(s).
+    Calculates the Cumulative BLEU score for a given sentence and reference(s).
       - references (list): List of reference sentences (list of words).
       - sentence (list): Candidate sentence (list of words) to be evaluated.
       - n (int): Maximum n-gram order for cumulative BLEU score calculation.
-      - bleu_score (float): Cumulative BLEU score for the candidate sentence.
-      - Initialize a list to store precision values for different
-        n-gram orders.
+      - float: Cumulative BLEU score for the candidate sentence.
+      - Initialize weights for different n-gram orders.
       - Iterate over different n-gram orders (up to the specified maximum).
-      - Initialize counters for n-grams in references and the
-        candidate sentence.
-      - Update counters with n-grams for references and the candidate sentence.
-      - Calculate the common n-grams between the candidate and references.
+      - Calculate n-grams for the candidate sentence.
+      - Iterate over reference sentences.
+      - Calculate n-grams for the reference sentence.
+      - Update the maximum n-gram counts.
+      - Clip the n-gram counts to the maximum counts.
       - Calculate precision for the current n-gram order.
-      - Append precision to the list.
-      - Calculate brevity penalty based on sentence length and minimum
-        reference length.
-      - Calculate cumulative BLEU score using precision values and
-        specified n-gram order.
-      - Return the calculated cumulative BLEU score.
+      - Calculate brevity penalty based on sentence length and
+        minimum reference length.
+      - Calculate cumulative BLEU score using precision values
+        and specified n-gram order.
     """
-    precisions = []
-    for i in range(1, n + 1):
-        reference_ngrams = Counter()
-        candidate_ngrams = Counter()
+    weights = [1.0/n] * n
+    bleu_scores = []
 
-        for reference in references:
-            reference_ngrams.update(
-                zip(*[reference[j:] for j in range(i)]))
+    for i in range(1, 1+n):
+        sentence_ngrams = calculate_ngrams(sentence, i)
+        sentence_counts = {
+            word: sentence_ngrams.count(word) for word in sentence_ngrams}
+        max_counts = {}
 
-        candidate_ngrams.update(
-            zip(*[sentence[j:] for j in range(i)]))
+        for ref in references:
+            ref_ngrams = calculate_ngrams(ref, i)
+            ref_counts = {word: ref_ngrams.count(word) for word in ref_ngrams}
+            for word in ref_counts:
+                max_counts[word] = max(
+                    max_counts.get(word, 0), ref_counts[word])
 
-        common_ngrams = candidate_ngrams & reference_ngrams
-        precision = sum(common_ngrams.values()) / max(
-            1, sum(candidate_ngrams.values()))
+        clipped_counts = {word: min(count, max_counts.get(word, 0)) for word,
+                          count in sentence_counts.items()}
 
-        precisions.append(precision)
+        bleu_score = sum(clipped_counts.values()) / max(sum(
+            sentence_counts.values()), 1)
+        bleu_scores.append(bleu_score)
 
-    brevity_penalty = min(1.0, len(sentence) / min(
-        len(ref) for ref in references))
-    bleu_score = brevity_penalty * math.exp(
-        sum(math.log(p) for p in precisions) / n)
+    closest_ref_len = min(len(ref) for ref in references)
 
-    return bleu_score
+    if len(sentence) > closest_ref_len:
+        brevity_penalty = 1
+    else:
+        brevity_penalty = np.exp(1 - closest_ref_len / len(sentence))
+
+    cumulative_bleu_score = brevity_penalty * np.exp(
+        sum(w*np.log(s) for w, s in zip(weights, bleu_scores)))
+
+    return cumulative_bleu_score
