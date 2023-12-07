650c6dc6c1b226e5b8af85c66eb5f47ac96b7c07
Julia Bullard <5534@holbertonstudents.com>
2023-12-06 21:47:43 -0600

    Task 1 Reworked.

--- supervised_learning/nlp_metrics/1-ngram_bleu.py
+++ supervised_learning/nlp_metrics/1-ngram_bleu.py
@@ -1,58 +1,64 @@
 #!/usr/bin/env python3
 """Task 1"""
 
-from collections import Counter
-import math
 import numpy as np
 
 
-def ngram_bleu(references, sentence, n):
+def calculate_ngrams(tokens, n):
     """
-    Calculates the N-gram BLEU score for a given sentence and reference(s).
-      - references (list): List of reference sentences (list of words).
-      - sentence (list): Candidate sentence (list of words) to be evaluated.
-      - n (int): Maximum n-gram order for N-gram BLEU score calculation.
-      - bleu_score (float): N-gram BLEU score for the candidate sentence.
-      - Initialize a list to store precision values for different
-        n-gram orders.
-      - Iterate over different n-gram orders (up to the specified maximum).
-      - Initialize counters for n-grams in references and the
-        candidate sentence.
-      - Update counters with n-grams for references and the candidate sentence.
-      - Calculate the common n-grams between the candidate and references.
-      - Calculate precision for the current n-gram order.
-      - Append precision to the list.
-      - Calculate brevity penalty based on sentence length and
-        minimum reference length.
-      - Calculate N-gram BLEU score using precision values and
-        average over different n-gram orders.
-      - Return the calculated N-gram BLEU score.
+    Helper function to calculate n-grams from a list of tokens.
+      - tokens (list): List of tokens (words).
+      - n (int): Size of the n-gram.
+      - list: List of n-grams.
+      - Generate n-grams using a sliding window of size 'n'.
     """
 
-    precisions = []
-    for i in range(1, n + 1):
-        reference_ngrams = []
-        sentence_ngrams = []
+    return [tuple(tokens[i:i+n]) for i in range(len(tokens) - n + 1)]
 
-        for ref in references:
-            ref_ngrams = [
-              tuple(ref[j:j + i]) for j in range(len(ref) - i + 1)]
-            reference_ngrams.extend(ref_ngrams)
 
-        sentence_ngrams = [
-          tuple(sentence[j:j + i]) for j in range(len(sentence) - i + 1)]
+def ngram_bleu(references, hypothesis, n):
+    """
+    Calculates the n-gram BLEU score for a sentence.
+      - references (list): List of reference translations.
+        Each reference translation is a list of words.
+      - hypothesis (list): List containing the model proposed sentence.
+      - n (int): Size of the n-gram to use for evaluation.
+      - float: The n-gram BLEU score.
+      - Calculate n-grams for the model proposed sentence.
+      - Initialize a dictionary to store maximum n-gram counts for each n-gram.
+      - Iterate over reference translations.
+      - Calculate n-grams for the reference translation.
+      - Update the maximum n-gram counts.
+      - Clip the n-gram counts to the maximum counts.
+      - Calculate precision for the n-gram order.
+      - Calculate brevity penalty based on sentence length and minimum
+        reference length.
+      - Calculate the final BLEU score.
+    """
 
-        overlap_count = sum(
-          1 for ngram in sentence_ngrams if ngram in reference_ngrams)
+    hypothesis_ngrams = calculate_ngrams(hypothesis, n)
+    hypothesis_counts = {
+        word: hypothesis_ngrams.count(word) for word in hypothesis_ngrams}
 
-        precision = overlap_count / max(len(sentence_ngrams), 1)
-        precisions.append(precision)
+    max_counts = {}
 
-    bleu = np.exp(np.sum(np.log(precisions)) / n)
+    for ref in references:
+        ref_ngrams = calculate_ngrams(ref, n)
+        ref_counts = {word: ref_ngrams.count(word) for word in ref_ngrams}
+        for word in ref_counts:
+            max_counts[word] = max(max_counts.get(word, 0), ref_counts[word])
 
-    closest_ref_length = min(
-      references, key=lambda ref: abs(len(ref) - len(sentence)))
-    brevity_penalty = min(1, len(sentence) / len(closest_ref_length))
+    clipped_counts = {word: min(count, max_counts.get(word, 0)) for word,
+                      count in hypothesis_counts.items()}
 
-    bleu_score = brevity_penalty * bleu
+    bleu_score = sum(clipped_counts.values()) / max(sum(
+        hypothesis_counts.values()), 1)
+
+    closest_ref_len = min(len(ref) for ref in references)
+
+    brevity_penalty = 1 if len(
+        hypothesis) > closest_ref_len else np.exp(
+            1 - closest_ref_len / len(hypothesis))
+
+    bleu_score = brevity_penalty * bleu_score
     return bleu_score
